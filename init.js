50a0724b6bc4225895e809fc9f3d06baa402747f
Anthony Ryan <anthonyryan1@gmail.com>
2014-09-14 02:53:15 -0400

    Refactor the javascript to be cleaner and more maintainable

--- init.js
+++ init.js
@@ -1,29 +1,30 @@
+/* jshint strict: false */
 /* global plugin, theUILang, noty */
+(function (plugin, theUILang, noty) {
+	// We can't use strict mode here because
+	// noty() triggers an eval :(
 
-/*
-Check for browser support and disable the plugin visibly if 
-the browser is unable to utilize this functionality.
+	var cookies = document.cookie.split(": "),
+		i;
 
-Prefix everything since we"re not able to use proper javascript
-scope due to poor design decisions in rutorrent.
-*/
+	// Lazy polyfill for browsers that don't yet support API change yet
+	if (typeof navigator.isProtocolHandlerRegistered === "undefined") {
+		navigator.isProtocolHandlerRegistered = function(){
+			return false;
+		};
+	}
 
-var magnetic_cookies = document.cookie.split(": "),
-	i;
+	// Mark this plugin as disabled if there's no browser support
+	// Purely cosmetic to help troubleshooting
+	if (typeof navigator.registerProtocolHandler === "undefined") {
+		plugin.disable();
+		return;
+	}
 
-if (typeof navigator.registerProtocolHandler === "undefined") {
-	plugin.disable();
-}
-
-
-else {
-
-	if (!(
-		typeof navigator.isProtocolHandlerRegistered === 'function' && 
-		navigator.isProtocolHandlerRegistered(
+	// If we're not the protcol handler, present the user with the choice
+	if (!navigator.isProtocolHandlerRegistered(
 			"magnet",
 			location.origin + location.pathname + "plugins/magnetic/adduri.php?uri=%s"
-		)
 	)) {
 		navigator.registerProtocolHandler(
 			"magnet",
@@ -31,20 +32,21 @@ else {
 			"ruTorrent (" + location.hostname + ")"
 		);
 	}
-}
 
-for (i = 0; i < magnetic_cookies.length; i++) {
-	if (magnetic_cookies[i].indexOf("magnetic_error=1") === 0) {
-		noty(theUILang.addTorrentFailed, "error");
-		document.cookie = "magnetic_error=; expires=" + new Date(0).toGMTString() + "; path=/";
-	}
-
-	if (magnetic_cookies[i].indexOf("magnetic_name=") === 0) {
-		if (magnetic_cookies[i] === "magnetic_name=1") {
-			noty(theUILang.addTorrentSuccess, "success");
-		} else {
-			noty(decodeURIComponent(magnetic_cookies[i].substring("magnetic_name=".length)) + " " + theUILang.addTorrentSuccess, "success");
+	// Show torrents added
+	for (i = 0; i < cookies.length; i++) {
+		if (cookies[i].indexOf("magnetic_error=1") === 0) {
+			noty(theUILang.addTorrentFailed, "error");
+			document.cookie = "magnetic_error=; expires=" + new Date(0).toGMTString() + "; path=/";
 		}
-		document.cookie = "magnetic_name=; expires=" + new Date(0).toGMTString() + "; path=/";
+
+		if (cookies[i].indexOf("magnetic_name=") === 0) {
+			if (cookies[i] === "magnetic_name=1") {
+				noty(theUILang.addTorrentSuccess, "success");
+			} else {
+				noty(decodeURIComponent(cookies[i].substring("magnetic_name=".length)) + " " + theUILang.addTorrentSuccess, "success");
+			}
+			document.cookie = "magnetic_name=; expires=" + new Date(0).toGMTString() + "; path=/";
+		}
 	}
-}
+})(plugin, theUILang, noty);
